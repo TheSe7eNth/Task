c1f30e6ca005bcee4bafaf6d617d08e3e6b1f6bf
TheSe7eNth <158582605+TheSe7eNth@users.noreply.github.com>
2024-08-15 19:45:35 +0800

    Create tasksheet-1-1-3.java

---
+++ tasksheet-1-1-3.java
@@ -0,0 +1,13 @@
+public class H3110 w0r1d {
+    public static void main(String[] args) {
+        char H = 'H';
+        byte three = 3;
+        int one = 1;
+        short zero = 0;
+        float twoPointZero = 2.0f;
+        boolean isTrue = true;
+
+        String output = "" + H + three + one + one + zero + " w" + zero + "r" + one + "d " + twoPointZero + " " + isTrue;
+        System.out.println(output);
+    }
+}
