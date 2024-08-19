75e1363aa26f5c39a45f3035c2dc361082cae82e
TheSe7eNth <158582605+TheSe7eNth@users.noreply.github.com>
2024-08-19 19:44:29 +0800

    done adding output

--- tasksheet115.java
+++ tasksheet115.java
@@ -1,1 +1,16 @@
+public class tasksheet115 {
+    public static void main(String[] args) {
+        String a = "Wow";
+        String b = "Wow";
+        String c = "Wow!";
+        String d = "Wow!";
 
+        boolean b1 = a == b;          // a == b (true, because both refer to the same string in the string pool)
+        boolean b2 = d.equals(b + "!"); // d.equals("Wow!") (true, "Wow!" == "Wow!")
+        boolean b3 = !c.equals(a);     // c = "Wow!" and a = "Wow", so they are not equal, making !c.equals(a) true
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
