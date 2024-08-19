d0db13bc0568108f4b1f99588bb6b3a6ecf015cd
TheSe7eNth <158582605+TheSe7eNth@users.noreply.github.com>
2024-08-19 12:14:46 +0000

    done adding output

--- tasksheet115.java
+++ tasksheet115.java
@@ -1,13 +1,13 @@
 public class tasksheet115 {
     public static void main(String[] args) {
-        String a = "Wow";
-        String b = "Wow";
-        String c = "Wow!";
-        String d = "Wow!";
+        String a = "Wow";        // a and b refer to the same interned string
+        String b = "Wow";        // a and b are the same object
+        String c = new String("Wow"); // c is a new String object
+        String d = b + "!";      // d is "Wow!"
 
-        boolean b1 = a == b;          // a == b (true, because both refer to the same string in the string pool)
-        boolean b2 = d.equals(b + "!"); // d.equals("Wow!") (true, "Wow!" == "Wow!")
-        boolean b3 = !c.equals(a);     // c = "Wow!" and a = "Wow", so they are not equal, making !c.equals(a) true
+        boolean b1 = a == b;          // true, because a and b are the same object
+        boolean b2 = d.equals(b + "!"); // true, because d is "Wow!" and b + "!" is also "Wow!"
+        boolean b3 = !c.equals(a);    // true, because c is not the same object as a
 
         if (b1 && b2 && b3) {
             System.out.println("Success!");
