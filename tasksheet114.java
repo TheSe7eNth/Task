ab05446800eee6c2cb67064bc06d35c0a777610e
TheSe7eNth <158582605+TheSe7eNth@users.noreply.github.com>
2024-08-19 15:00:17 +0800

    done adding output

--- tasksheet114.java
+++ tasksheet114.java
@@ -1,9 +1,9 @@
 public class tasksheet114 {
     public static void main(String[] args) {
-        int check_number = 10;
+        int bogsnyaw = 10;
         String message;
 
-        for (int i = 1; i <= check_number; i++) {
+        for (int i = 1; i <= bogsnyaw; i++) {
             message = (i % 2 == 0) ? i + " is an even number" : i + " is an odd number";
             System.out.println(message);
         }
